539b82430ccc8e4912d6698f1d7c8e91b2ee3c65
Harveen G <harveen096@revature.net>
2022-11-14 14:23:21 -0800

    sign in created

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalServiceService } from '../Service/global-service.service';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPageComponent implements OnInit {
 
-  constructor() { }
+  name: string="";
+  email: string="";
+  password: string="";
+  message: string="";
+
+  constructor(public router: Router, public gservice: GlobalServiceService) { }
 
   ngOnInit(): void {
   }
 
+  addUser(){
+    this.gservice.addUser(this.name,this.email,this.password).subscribe(data=>
+      {console.log(data);
+        if(data){
+          this.message="success"
+        }else{
+          this.message="already exists"
+        }
+      })
+  }
+
+  logIn(){
+    this.router.navigateByUrl('');
+  }
 }
